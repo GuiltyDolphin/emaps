728eb6eeae46194f32a01bafa349ece520bc7a10
Ben Moon <guiltydolphin@gmail.com>
2016-09-10 13:21:58 +0100

    Remove ';;;' from commentary line for consistency

--- emaps.el
+++ emaps.el
@@ -1,5 +1,5 @@
 ;;; emaps.el --- utilities for working with keymaps.
-;;;
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
