7c561f3ded2015ed3774e5784059d6601082743e
Ben Moon <software@guiltydolphin.com>
2020-05-08 18:59:52 +0100

    Release version 0.2.0

--- emaps.el
+++ emaps.el
@@ -18,7 +18,7 @@
 ;; URL: https://github.com/GuiltyDolphin/emaps
 ;; Git-Repository: git://github.com/GuiltyDolphin/emaps.git
 ;; Created: 2016-06-20
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Keywords: convenience, keyboard, keymap, utility
 ;; Package-Requires: ((dash "2.17.0") (emacs "24"))
 
